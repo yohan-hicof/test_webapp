e425915e4ba4982d393b9df17aa9c94c9454e437
yohan-hicof <56432427+yohan-hicof@users.noreply.github.com>
2019-10-11 10:56:47 +0200

    First files to test the webapp

---
+++ app.js
@@ -0,0 +1,34 @@
+
+//Set contraints for the video stream
+var constraints = {video: {facingMode: "user"}, audio: false};
+
+// Define contants
+const   cameraView = document.querySelector("#camera--view"),
+	cameraOutput = document.querySelector("#Camera--output"),
+	cameraSensor = document.querySelector("#camera--sensor"),
+	cameraTrigger = document.querySelector("#camera--trigger")
+
+//Access the device camera and stream to cameraView
+function cameraStart(){
+	navigator .mediaDevices
+		.getUserMedia(contraints)
+		then(function(stream){
+		track = stream.getTracks()[0];
+		cameraView.srcObject = stream;
+	})
+	.catch(function(error){
+		console.error("Error, something went wrong.",error);
+	});
+}
+
+//Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+	cameraSensor.width = cameraView.videoWidth;
+	cameraSensor.height = cameraView.videoHeight;
+	cameraSensor.getContext("2d").drawImage(cameraView,0,0);
+	cameraOutput.src = cameraSensor.toDataUrl("image/webp");
+	cameraOutput.classList.add("taken");
+}
+
+//Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
