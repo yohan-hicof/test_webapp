24682b59b0685874f78b708bddf682027a4881d3
yohan-hicof <56432427+yohan-hicof@users.noreply.github.com>
2019-10-11 11:39:33 +0200

    corrections

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
-
-//Set contraints for the video stream
-var constraints = {video: {facingMode: "user"}, audio: false};
-
-// Define contants
-const   cameraView = document.querySelector("#camera--view"),
-	cameraOutput = document.querySelector("#Camera--output"),
-	cameraSensor = document.querySelector("#camera--sensor"),
-	cameraTrigger = document.querySelector("#camera--trigger")
-
-//Access the device camera and stream to cameraView
-function cameraStart(){
-	navigator .mediaDevices
-		.getUserMedia(contraints)
-		then(function(stream){
-		track = stream.getTracks()[0];
-		cameraView.srcObject = stream;
-	})
-	.catch(function(error){
-		console.error("Error, something went wrong.",error);
-	});
-}
-
-//Take a picture when cameraTrigger is tapped
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}// Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-	cameraSensor.width = cameraView.videoWidth;
-	cameraSensor.height = cameraView.videoHeight;
-	cameraSensor.getContext("2d").drawImage(cameraView,0,0);
-	cameraOutput.src = cameraSensor.toDataUrl("image/webp");
-	cameraOutput.classList.add("taken");
-}
-
-//Start the video stream when the window loads
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
